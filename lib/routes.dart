c4d021c2dad4a984953b8ea9be6836417efcb976
Karine Gilberti <gilbertikarine22@gmail.com>
2025-05-27 10:50:03 -0300

    Autenticação de login iniciada

---
+++ lib/routes.dart
@@ -0,0 +1,4 @@
+class Routes {
+  static const String urlSignInWithCustomToken =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyCvQhOvSTgnam7O5M6xG0oGYZsJIHAUa60";
+}
---
+++ lib/sing_up.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+
+class SingUpService {
+  singUp(String email, String password){
+
+  }
+}
